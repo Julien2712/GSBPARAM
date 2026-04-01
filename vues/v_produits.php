be551b30ca95531cde9242fedf6203f5f478b5f6
Capybaraw <superefe45500@gmail.com>
2026-04-01 16:54:29 +0200

    produit et leur info v2

--- vues/v_produits.php
+++ vues/v_produits.php
@@ -20,10 +20,10 @@ if (isset($erreurFiltre)) {
 				<div class="descrCard"><a target="_blank" href="index.php?uc=voirProduits&produit=<?= $id ?>&action=voirDetails" style="text-decoration:none; color:inherit;"><?= htmlspecialchars($description ?? '', ENT_QUOTES, 'UTF-8') ?></a></div>
 				<div class="prixCard"><?= htmlspecialchars($prix ?? '', ENT_QUOTES, 'UTF-8') . " €" ?></div>
 			</div>
-			<div style="display: flex; justify-content: space-between; align-items: center; padding: 0 10px;">
+			<div class="card-actions">
 				<div class="imgCard"><a href="index.php?uc=gererPanier&produit=<?= $id ?>&action=ajouterAuPanier">
 						<img src="assets/images/mettrepanier.png" title="Ajouter au panier" alt="Mettre au panier"> </a></div>
-				<a target="_blank" href="index.php?uc=voirProduits&produit=<?= $id ?>&action=voirDetails" style="background-color: #28a745; color: white; padding: 5px 10px; text-decoration: none; border-radius: 5px; font-size: 14px;">Infos du produit</a>
+				<a target="_blank" href="index.php?uc=voirProduits&produit=<?= $id ?>&action=voirDetails" class="btn-info-produit">Infos du produit</a>
 			</div>
 		</div>
 		<?php
